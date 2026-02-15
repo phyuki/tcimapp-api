776049d1b11e9c68f8311a754f7b5b931ed90cef
phyuki <pedrogoes06@hotmail.com>
2026-02-15 16:49:14 -0300

    feat: Add controle por variaveis de ambiente

--- index.js
+++ index.js
@@ -1,58 +1,70 @@
-const express = require("express")
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const Sequelize = require('sequelize')
-const model = require('./src/models')
-const port = process.env.PORT || 3000
+require("dotenv").config();
 
-const authRoutes = require('./src/routes/auth.routes');
-const professionalRoutes = require('./src/routes/professional.routes');
-const patientRoutes = require('./src/routes/patient.routes');
-const reportRoutes = require('./src/routes/report.routes');
-const dassRoutes = require('./src/routes/dass.routes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Sequelize = require("sequelize");
+const model = require("./src/models");
+const port = process.env.PORT || 3000;
 
-let app = express()
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const authRoutes = require("./src/routes/auth.routes");
+const professionalRoutes = require("./src/routes/professional.routes");
+const patientRoutes = require("./src/routes/patient.routes");
+const reportRoutes = require("./src/routes/report.routes");
+const dassRoutes = require("./src/routes/dass.routes");
 
-app.use('/auth', authRoutes);
-app.use('/professionals', professionalRoutes);
-app.use('/patients', patientRoutes);
-app.use('/reports', reportRoutes);
-app.use('/dass', dassRoutes);
+let app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/teste', async(req,res) => {
-     res.send(JSON.stringify('OK!'))
-})
+app.use("/auth", authRoutes);
+app.use("/professionals", professionalRoutes);
+app.use("/patients", patientRoutes);
+app.use("/reports", reportRoutes);
+app.use("/dass", dassRoutes);
 
-app.get('/disorders', async(req,res) => {
+app.get("/teste", async (req, res) => {
+  res.send(JSON.stringify("OK!"));
+});
 
-    const exists = await model.scidQuestions.findAll({
-         where: { disorder: req.query.disorder }
-    })
+app.get("/disorders", async (req, res) => {
+  const exists = await model.scidQuestions.findAll({
+    where: { disorder: req.query.disorder },
+  });
 
-    if(exists){
-         const allItems = exists.map(item => [item.dataValues.id, item.dataValues.cod, item.dataValues.question])
-         res.json(allItems)
-    }
-})
+  if (exists) {
+    const allItems = exists.map((item) => [
+      item.dataValues.id,
+      item.dataValues.cod,
+      item.dataValues.question,
+    ]);
+    res.json(allItems);
+  }
+});
 
-app.post('/details', async(req,res) => {
-    
-    const allCriteria = req.body.criteria.map((value) => (value === undefined ? null : value))
-    const allScores = req.body.score.map((value) => (value === undefined ? null : value))
+app.post("/details", async (req, res) => {
+  const allCriteria = req.body.criteria.map((value) =>
+    value === undefined ? null : value,
+  );
+  const allScores = req.body.score.map((value) =>
+    value === undefined ? null : value,
+  );
 
-    const reportDetails = allCriteria.map((criteria, ind) => ({criteria, score: allScores[ind], 
-                                                      disorder: req.body.disorder, patientId: req.body.patientId}));
+  const reportDetails = allCriteria.map((criteria, ind) => ({
+    criteria,
+    score: allScores[ind],
+    disorder: req.body.disorder,
+    patientId: req.body.patientId,
+  }));
 
-    const report = await model.scidcriteria.bulkCreate(reportDetails)
-    
-    if(report){
-         res.send(JSON.stringify(report))
-    }
-})
+  const report = await model.scidcriteria.bulkCreate(reportDetails);
+
+  if (report) {
+    res.send(JSON.stringify(report));
+  }
+});
 
 app.listen(port, () => {
-    console.log("Servidor está rodando...")
-})
+  console.log("Servidor está rodando...");
+});
--- src/routes/dass.routes.js
+++ src/routes/dass.routes.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const DassController = require('../controllers/dass.controller');
+const DassController = require("../controllers/dass.controller");
 
-router.get('/', DassController.getQuestions);
-router.post('/', DassController.saveScore);
-router.get('/scores', DassController.getScores);
-router.post('/answers', DassController.saveAnswers);
+router.get("/", DassController.getQuestions);
+router.post("/", DassController.saveScore);
+router.get("/scores", DassController.getScores);
+router.post("/answers", DassController.saveAnswers);
 
 module.exports = router;
